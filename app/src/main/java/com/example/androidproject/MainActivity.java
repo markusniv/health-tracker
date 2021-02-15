3395eb8f6b6fa6f7857245ed3ffbffe39d9a1874
Markus Nivasalo <markus.nivasalo@outlook.com>
2021-02-15 11:27:32 +0200

    Explanatory commit on branches.

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -8,9 +8,13 @@ public class MainActivity extends AppCompatActivity {
 
     private int testingInt = 6;
 
+    private double testingDouble = 2.1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testingInt = 124;
     }
 }
