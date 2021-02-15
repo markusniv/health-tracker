cb8b105c3e67e9a4f5247afec337ecaae9f1e961
Markus Nivasalo <markus.nivasalo@outlook.com>
2021-02-15 10:52:41 +0200

    Explanatory commit for teaching purposes.

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int testVariable = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testVariable = 7;
     }
 }
