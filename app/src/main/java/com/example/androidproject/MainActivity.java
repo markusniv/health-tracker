74dbd032fa36a5245d344bde80e6b9d83cb159f6
Markus Nivasalo <markus.nivasalo@outlook.com>
2021-02-15 11:06:32 +0200

    Explanatory commit for teaching purposes.

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String testString = "This is a test.";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
