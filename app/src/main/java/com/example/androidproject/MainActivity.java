55f2fd837430afd949415e9deb358aa221f0904c
Ilkka Karvinen <ilkka.karvinen@gmail.com>
2021-02-15 11:08:22 +0200

    Learning GIT

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //jotain kommentteja
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
