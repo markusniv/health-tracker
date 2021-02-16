daf52edc67d10a377ae413b5f61c41b77c3123db
Markus Nivasalo <markus.nivasalo@outlook.com>
2021-02-16 11:10:13 +0200

    Added GenderChooseActivity on first launch of the app to store information into SharedPreferences on user's gender, added some basic layout on MainActivity

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -2,19 +2,31 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
-    private int testingInt = 6;
-
-    private double testingDouble = 2.1;
+    public static final String EXTRA = "com.example.androidproject.EXTRA";
+    final String PREFS_NAME = "PreferencesFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testingInt = 124;
+        SharedPreferences prefGet = getSharedPreferences(PREFS_NAME, Activity.MODE_PRIVATE);
+
+        // If launching for the first time, move to GenderChooseActivity to determine user gender
+        if (prefGet.getBoolean("FIRST_USER_LAUNCH", true)) {
+            Log.d("Note", "First time launched");
+
+            Intent getGender = new Intent(MainActivity.this, GenderChooseActivity.class);
+            getGender.putExtra(EXTRA, PREFS_NAME);
+            startActivity(getGender);
+        }
+
     }
 }
