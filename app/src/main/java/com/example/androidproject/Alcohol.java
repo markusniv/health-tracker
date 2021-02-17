444c425a5f497559da565f75c45fa21057080bb8
Markus Nivasalo <markus.nivasalo@outlook.com>
2021-02-17 15:05:27 +0200

    Added methods to basic data classes

--- app/src/main/java/com/example/androidproject/Alcohol.java
+++ app/src/main/java/com/example/androidproject/Alcohol.java
@@ -5,11 +5,13 @@ public class Alcohol extends Vice {
     private String name;
     private int size;
     private double volPercent;
+    private double price;
 
-    public Alcohol(String name, int size, double volPercent) {
+    public Alcohol(String name, int size, double volPercent, double price) {
         this.name = name;
         this.size = size;
         this.volPercent = volPercent;
+        this.price = price;
     }
 
     public String getName() {
@@ -22,6 +24,10 @@ public class Alcohol extends Vice {
 
     public double getVolPercent() {
         return volPercent;
+    }
+
+    public double getPrice() {
+        return price;
     }
 
 }
--- app/src/main/java/com/example/androidproject/TobaccoProduct.java
+++ app/src/main/java/com/example/androidproject/TobaccoProduct.java
@@ -4,10 +4,12 @@ public class TobaccoProduct extends Vice {
 
     private String name;
     private double nicotineAmount;
+    private double price;
 
-    public TobaccoProduct(String name, double nicotineAmount) {
+    public TobaccoProduct(String name, double nicotineAmount, double price) {
         this.name = name;
         this.nicotineAmount = nicotineAmount;
+        this.price = price;
     }
 
     public String getName() {
@@ -16,6 +18,10 @@ public class TobaccoProduct extends Vice {
 
     public double getNicotineAmount() {
         return nicotineAmount;
+    }
+
+    public double getPrice() {
+        return price;
     }
 
 }
