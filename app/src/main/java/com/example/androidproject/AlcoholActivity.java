bddcc7bbe83abc9edac05f171e176d24cbc05f81
leinonenko <Kabaman1995p>
2021-03-02 10:30:06 +0200

    AlcoholActivity updated

--- app/src/main/java/com/example/androidproject/AlcoholActivity.java
+++ app/src/main/java/com/example/androidproject/AlcoholActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class AlcoholActivity extends AppCompatActivity {
+
+    private Alcohol alcohol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,27 @@ public class AlcoholActivity extends AppCompatActivity {
 
         alcoholList.setAdapter(new ArrayAdapter<Alcohol>(this,
                 android.R.layout.simple_list_item_1,
-                AlcoholSingleton.getAlcoholInstance().getDrinks()));
+                AlcoholSingleton.getAlcoholInstance().getDrinks()
+        ));
 
+        alcoholList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                alcohol = AlcoholSingleton.getAlcoholInstance().getDrinks().get(position);
+            }
+        });
+    }
+    public void onClick(View v)     {
+        EditText alcoholPriceField = findViewById(R.id.priceField);
+        String alcoholPriceString = alcoholPriceField.getText().toString();
+
+        double alcoholPrice = Double.parseDouble(alcoholPriceString);
+        AddAlcohol addAlcoholEvent = new AddAlcohol(alcohol, alcoholPrice);
+        EventSingleton.getEventInstance().AddViceEvent(addAlcoholEvent);
+
+        Log.i("AddAlcohol event", EventSingleton.getEventInstance().getViceEventList().toString());
+
+        Intent intent = new Intent(AlcoholActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
